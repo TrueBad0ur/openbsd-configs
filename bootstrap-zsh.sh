dd112de0ffeeab04ebc8ab6542a43e3d3a471478
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2026-05-30 11:04:50 +0000

    Update zsh

--- bootstrap-zsh.sh
+++ bootstrap-zsh.sh
@@ -7,11 +7,14 @@ doas pkg_add zsh curl fastfetch
 echo "==> installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+echo "==> installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 echo "==> writing .zshrc..."
 cat > ~/.zshrc << 'ZSHRC'
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="dieter"
-plugins=(git sudo)
+plugins=(zsh-autosuggestions)
 source $ZSH/oh-my-zsh.sh
 
 export CLICOLOR=1
