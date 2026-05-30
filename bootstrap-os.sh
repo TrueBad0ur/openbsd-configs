7e53bdd4ade511dd03a473cf402e74bdfacf52a6
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2026-05-30 14:08:43 +0300

    Update timezone

--- bootstrap-os.sh
+++ bootstrap-os.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
-echo "==> renaming host"
+echo "==> renaming host..."
 echo "thinkpad" | doas tee /etc/myname
+echo "==> done"
+
+echo "==> changing timezone..."
+doas ln -fs /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 echo "==> done"
